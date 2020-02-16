a906ce3b672b549aed7c121e6486e7b5393a2515
Mateusz Miękicki <miekickimateusz@gmail.com>
2020-02-16 02:04:23 +0100

    basic set build script

---
+++ clock/build.py
@@ -0,0 +1,12 @@
+
+
+nameFile = "main"
+portName = "COM6"
+speedBand = "115200"
+cmdSet = ["avr-gcc -Os -Wall -Wextra -std=c11 -DF_CPU=16000000UL -mmcu=atmega328p -c -o "+nameFile+".o "+nameFile+".c",
+"avr-gcc -mmcu=atmega328p "+nameFile+".o "+"-o "+nameFile,
+"avr-objcopy -O ihex -R .eeprom "+nameFile+" "+nameFile+".hex",
+"avrdude -F -V -c arduino -p ATMEGA328P -P "+ portName+" -b "+speedBand+" "+"-U flash:w:"+nameFile+".hex"]
+
+for x in cmdSet:
+    print(x)
